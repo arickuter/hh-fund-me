ff1294f108f8260b0f97ec80c8aeaec148849345
Aric Kuter <arickuter99@gmail.com>
2022-08-24 13:57:00 +0200

    complete deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,31 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
+import "dotenv/config";
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY!;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: { compilers: [{ version: "0.8.9" }, { version: "0.6.6" }] },
   defaultNetwork: "hardhat",
+  networks: {
+    rinkeby: { url: RINKEBY_RPC_URL, accounts: [PRIVATE_KEY], chainId: 4 },
+    localhost: { url: "http://127.0.0.1:8545/", chainId: 31337 },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "ETH",
+  },
   namedAccounts: {
     deployer: {
       default: 0,
---
+++ helper_hardhat_config.ts
@@ -0,0 +1,25 @@
+export interface networkConfigItem {
+  ethUsdPriceFeed?: string;
+  blockConfirmations?: number;
+}
+
+export interface networkConfigInfo {
+  [key: string]: networkConfigItem;
+}
+
+export const networkConfig: networkConfigInfo = {
+  localhost: {},
+  hardhat: {},
+  // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
+  // Default one is ETH/USD contract on Kovan
+  kovan: {
+    ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
+    blockConfirmations: 5,
+  },
+  rinkeby: {
+    ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+    blockConfirmations: 5,
+  },
+};
+
+export const developmentChains = ["hardhat", "localhost"];
